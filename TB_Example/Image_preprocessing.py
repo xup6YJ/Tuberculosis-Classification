4d67806f4ab5c34582938147641205c4d1d1aebf
Lin Y J <74194952+xup6YJ@users.noreply.github.com>
2021-07-10 01:14:13 +0800

    Update Image_preprocessing.py

--- TB_Example/Image_preprocessing.py
+++ TB_Example/Image_preprocessing.py
@@ -1,10 +1,4 @@
 # -*- coding: utf-8 -*-
-"""
-Created on Tue Jul  6 15:30:38 2021
-
-@author: Lin
-"""
-
 
 #Image Preprocessing
 
@@ -24,7 +18,8 @@ test_df = pd.read_csv(test_csv_path)
 
 #Plot Example
 i = 1
-im = cv2.imread('D:/File_X/Graduate/TB/MergeTB/TB (1).png')
+img_path = os.path.join(os.getcwd(), 'MergeTB/TB (1).png')
+im = cv2.imread(img_path)
 img3 = IF.crop_up_down_equalizeHist(im, train = True)
 
 plt.imshow(im, cmap='gray', interpolation = 'bilinear')
@@ -38,21 +33,21 @@ plt.show()
 #Image Preprocess
 
 #Transfer file path
-original_dir = 'D:/File_X/Graduate/TB/Modeling'
+original_dir = os.path.join(os.getcwd(), 'Modeling')
 if not os.path.isdir(original_dir): os.mkdir(original_dir)
-train_dir = 'D:/File_X/Graduate/TB/Modeling/train'
+train_dir = os.path.join(original_dir, 'train')
 if not os.path.isdir(train_dir): os.mkdir(train_dir)
-test_dir = 'D:/File_X/Graduate/TB/Modeling/test'
+test_dir = os.path.join(original_dir, 'test')
 if not os.path.isdir(test_dir): os.mkdir(test_dir)
 
-train_normal_dir = train_dir + '/train_normal'
+train_normal_dir = os.path.join(train_dir, 'train_normal')
 if not os.path.isdir(train_normal_dir): os.mkdir(train_normal_dir)   
-train_tb_dir = train_dir + '/train_tb'
+train_tb_dir = os.path.join(train_dir, 'train_tb')
 if not os.path.isdir(train_tb_dir): os.mkdir(train_tb_dir)
      
-test_normal_dir = test_dir + '/test_normal'
+test_normal_dir = os.path.join(test_dir, 'test_normal')
 if not os.path.isdir(test_normal_dir): os.mkdir(test_normal_dir)
-test_tb_dir = test_dir + '/test_tb'
+test_tb_dir = os.path.join(test_dir, 'test_tb')
 if not os.path.isdir(test_tb_dir): os.mkdir(test_tb_dir)
 
 x_train = train_df.Filepath
@@ -64,6 +59,8 @@ fail_list = []
 #Train
 for i in range(len(train_df)):
     
+    #27 may need to modify
+    #find the letter 'N' in your path, the number 27 means 'N' is at the 27th letter of path
     if x_train[i][27]  == 'N':  #Normal
             
         img = cv2.imread(x_train[i])
@@ -81,8 +78,9 @@ for i in range(len(train_df)):
         img2 = IF.crop_up_down_equalizeHist(img, train = True)
         img3 = IF.crop_up_down_equalizeHist(img, train = False)
         img4 = IF.random_crop_equalizeHist(img, resize = 256, train = True)
-
-        new_filename = os.path.join(train_tb_dir, x_train[i][30:])
+        
+        #30 may need to modify, example filename will be 'filename.png'
+        new_filename = os.path.join(train_tb_dir, x_train[i][30:])  
         new_filename2 = os.path.join(train_tb_dir, x_train[i][30:-4]+ '_2.png')
         new_filename3 = os.path.join(train_tb_dir, x_train[i][30:-4]+ '_3.png')
         
@@ -107,7 +105,7 @@ for i in range(len(test_df)):
             
         img = cv2.imread(x_test[i])
         img2 = IF.crop_up_down_equalizeHist(img)
-        new_filename = os.path.join(test_normal_dir, x_test[i][34:])
+        new_filename = os.path.join(test_normal_dir, x_test[i][34:]) #34 may need to modify
         status = cv2.imwrite(new_filename, img2)
         if status == False:
             fail_list.append(x_test[i])
@@ -115,7 +113,7 @@ for i in range(len(test_df)):
     else:
         img = cv2.imread(x_test[i]) 
         img2 = IF.crop_up_down_equalizeHist(img)
-        new_filename = os.path.join(test_tb_dir, x_test[i][30:])
+        new_filename = os.path.join(test_tb_dir, x_test[i][30:]) #30 may need to modify
         status = cv2.imwrite(new_filename, img2)
 
         if status == False :

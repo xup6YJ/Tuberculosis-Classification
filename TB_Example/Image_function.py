ebd6815981d17cb6fcd613594d7db546b0b002c7
Lin Y J <74194952+xup6YJ@users.noreply.github.com>
2021-07-10 01:17:04 +0800

    Update Image_function.py

--- TB_Example/Image_function.py
+++ TB_Example/Image_function.py
@@ -1,9 +1,4 @@
 # -*- coding: utf-8 -*-
-"""
-Created on Tue Jul  6 15:40:49 2021
-
-@author: Lin
-"""
 
 import cv2
 from keras.preprocessing.image import ImageDataGenerator, load_img, img_to_array, array_to_img
